2638f7f5b0b6de5132c1db437938cfb53ea7ba66
Shital Shah <shitals@microsoft.com>
2018-05-08 04:10:27 -0700

    boosting exp1

---
+++ NNExp/results/pytorch/mnist_cnn_bneural.py
@@ -0,0 +1,11 @@
+Test set: Average loss: 0.1427, Accuracy: 9511/10000 (95.11%)
+Test set: Average loss: 0.0961, Accuracy: 9705/10000 (97.05%)
+Test set: Average loss: 0.0835, Accuracy: 9743/10000 (97.43%)
+Test set: Average loss: 0.0689, Accuracy: 9778/10000 (97.78%)
+Test set: Average loss: 0.0710, Accuracy: 9797/10000 (97.97%)
+Test set: Average loss: 0.0570, Accuracy: 9813/10000 (98.13%)
+Test set: Average loss: 0.0561, Accuracy: 9843/10000 (98.43%)
+Test set: Average loss: 0.0600, Accuracy: 9819/10000 (98.19%)
+Test set: Average loss: 0.0570, Accuracy: 9811/10000 (98.11%)
+Test set: Average loss: 0.0494, Accuracy: 9838/10000 (98.38%)
+Press any key to continue . . .
